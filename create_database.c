56e2bfa8f6110c8cd2381651200510ae4b26be58
Aditya Shirawal <151901683+Aadivishwakarm@users.noreply.github.com>
2026-03-29 21:16:14 +0530

    Refactor create_database function

--- create_database.c
+++ create_database.c
@@ -1,14 +1,6 @@
 #include "head.h"
 #include <ctype.h>
 
-// void to_lowercase(char *str)
-// {
-//     for (int i = 0; str[i]; i++)
-//     {
-//         str[i] = tolower(str[i]);
-//     }
-// }
-
 int create_database(file_list **hash, char *filename)
 {
     FILE *fp = fopen(filename, "r");
